6a55fdbc6e85dd78f4167673f87a536c248f6d64
houda-el <houda.eloijaji@gmail.com>
2025-05-11 15:57:59 +0200

    Update mission3.c

--- mission3.c
+++ mission3.c
@@ -97,3 +97,43 @@ void Endommager(Route *graphe[N_SOMMETS][N_SOMMETS]) // endommage des villes de 
         graphe[sommet1][sommet2]->est_endommagee = true;
     }
 }
+
+void Djikstra_endommagement(Route *graphe[N_SOMMETS][N_SOMMETS], int Distance[], int SommetDepart, int Predecesseur[])
+{
+    // Ici les longueurs sont positives ou nulles, l'algorithme de Djikstra est donc applicable pour determiner les plus courts chemins a partir du sommet de depart
+    // Dans cette version : nouvelles villes endommagees non-accessibles et villes endommagees a la base egalement
+    // Initialisation
+    for(int i = 0; i < N_SOMMETS; i ++)
+    {
+        Distance[i] = Infini;
+        Predecesseur[i] = AUCUN_Predecesseur; // Pas de predecesseur au debut de la recherche
+    }
+    Distance[SommetDepart] = 0; // Distance Depart-Depart = 0
+
+    bool visite[N_SOMMETS] = {false}; // Aucun sommet visite pour l'instant
+
+    for(int passage = 0; passage < N_SOMMETS; passage ++)
+    {
+        // Obtenir sommet non-visite avec la plus courte distance 
+        int LeSommet = Sommet_DistanceMinimale(Distance, visite);
+
+        visite[LeSommet] = true; // Ce sommet devient visite
+        
+        // Explorer les voisins du sommet actuel
+        for(int voisin = 0; voisin < N_SOMMETS; voisin ++)
+        {
+            if(graphe[LeSommet][voisin] != NULL && visite[voisin] == false) // S'il existe une route et que le voisin n'a pas encore ete visite
+            {
+                if(graphe[LeSommet][voisin]->etat == NON_ENDOMMAGEE)
+                {
+                    int nouvelle_distance = graphe[LeSommet][voisin]->distance + Distance[LeSommet];
+                    if(nouvelle_distance < Distance[voisin])
+                    {
+                        Distance[voisin] = nouvelle_distance; // Actualisation de la distance
+                        Predecesseur[voisin] = LeSommet;
+                    } 
+                }
+            }
+        }
+    }
+}
