77dd0922c7f1c35221c3aaeabc175a8eebc3a48e
MayaMeziane213 <meziane@et.esiea.fr>
2025-05-12 17:42:20 +0200

    Create mission2.c

---
+++ mission2.c
@@ -0,0 +1,30 @@
+void dfs(bool* visite, int sommet, Graphe* g) {
+    visite[sommet] = true;
+    Route* route = g->noeud[sommet].liste_adjacence;
+    while (route != NULL) {
+        int voisin = route->destination;
+        if (route->etat != DETRUITE && !visite[voisin]) {
+            dfs(visite, voisin, g);
+        }
+        route = route->frere;
+    }
+}
+
+void trouverGroupesChemins(Graphe* g) {
+    bool visite[N_SOMMETS] = {false};
+    int groupe = 1;
+    for (int i = 0; i < g->Ordre; i++) {
+        if (!visite[i]) {
+            printf("Groupe %d : ", groupe);
+            dfs(visite, i, g);
+            for (int j = 0; j < g->Ordre; j++) {
+                if (visite[j]) {
+                    printf("%s (%d) ", g->noeud[j].sommet.nom, j);
+                    visite[j] = false;
+                }
+            }
+            printf("\n");
+            groupe++;
+        }
+    }
+}
